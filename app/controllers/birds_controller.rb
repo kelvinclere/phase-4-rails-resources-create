c0700471574b957d2f2fd0fb705da1f8f1f0ba74
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-11-23 20:12:11 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,11 +9,13 @@ class BirdsController < ApplicationController
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    render json: bird
+
+    end
+
+    # POST /birds
+    def create
+      bird = Bird.create(name: params[:name], species: params[:species])
+      render json: bird, status: :created
     end
   end
-
-end
